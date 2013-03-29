00cdc9600e044ba929596d433ba7a01f3da7629e
Nick and Peter <student@appacademy.io>
2013-03-28 23:54:22 -0700

    Updated action_model.rb

--- questions/action_model.rb
+++ questions/action_model.rb
@@ -3,13 +3,4 @@ require_relative 'model'
 class QuestionAction < Model
   attr_accessible :type, :time
   VALID_ACTIONS = ['retract', 'close', 'reopen']
-
-  def initialize(type, time)
-    @type = type
-    @time = time
-  end
-
-  def self.parse_hash(hash)
-    QuestionAction.new(hash['type'], hash['time'])
-  end
 end
